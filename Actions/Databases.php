b2dfdaf9987b5cbdd3763b2ead8c82bb1bd37c96
Yvanne Sniper <rosatyvann@gmail.com>
2024-02-28 12:01:22 +0100

    connexion réussie a bdd

--- Actions/Databases.php
+++ Actions/Databases.php
@@ -1,19 +1,17 @@
 <?php
-$serveur = 'mysql.yvannerosat.com';
-$port = '3306';
+$serveur = 'yvannerosat.com';
 $utilisateur = 'u479250329_UserTransfer';
 $mot_de_passe = 'V]O*3w|L82o';
 $bdd = 'u479250329_oui_transfer';
 
 try {
-    $connexion = new PDO("mysql:host=$serveur;port=$port;dbname=$bdd", $utilisateur, $mot_de_passe);
+    $connexion = new PDO("mysql:host=$serveur;dbname=$bdd", $utilisateur, $mot_de_passe);
 
-if($connexion) {
-echo "Connexion réussi";
-} else {
-    echo "Erreur de connexion";
-}
-
-} catch(PDOException $e) {
-echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    if ($connexion) {
+        echo "Connexion réussi";
+    } else {
+        echo "Erreur de connexion";
+    }
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
 }
