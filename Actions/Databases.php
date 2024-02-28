0e0613878a7dd2bef7be1082059380a0cad9f98a
Yvanne Sniper <rosatyvann@gmail.com>
2024-02-28 12:03:21 +0100

    fonction connexion

--- Actions/Databases.php
+++ Actions/Databases.php
@@ -1,17 +1,22 @@
 <?php
-$serveur = 'yvannerosat.com';
-$utilisateur = 'u479250329_UserTransfer';
-$mot_de_passe = 'V]O*3w|L82o';
-$bdd = 'u479250329_oui_transfer';
 
-try {
-    $connexion = new PDO("mysql:host=$serveur;dbname=$bdd", $utilisateur, $mot_de_passe);
+function connexion()
+{
 
-    if ($connexion) {
-        echo "Connexion réussi";
-    } else {
-        echo "Erreur de connexion";
+    $serveur = 'yvannerosat.com';
+    $utilisateur = 'u479250329_UserTransfer';
+    $mot_de_passe = 'V]O*3w|L82o';
+    $bdd = 'u479250329_oui_transfer';
+
+    try {
+        $connexion = new PDO("mysql:host=$serveur;dbname=$bdd", $utilisateur, $mot_de_passe);
+
+        if ($connexion) {
+            echo "Connexion réussi";
+        } else {
+            echo "Erreur de connexion";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de connexion à la base de données : " . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "Erreur de connexion à la base de données : " . $e->getMessage();
 }
