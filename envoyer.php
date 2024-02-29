08a8cc781323490ad42da4cae771581e9210d83e
Aboubakr <aboubakr.zennir@gmail.com>
2024-02-29 11:41:12 +0100

    envoyer

--- envoyer.php
+++ envoyer.php
@@ -7,3 +7,20 @@ require('./HeaderFooter/Header.php');
 ?>
 
 <h1 style='text-align:center; '>Envoyer un fichier</h1>
+
+    <!--
+    Pour envoyer un fichier :
+    - On utilisera la méthode POST parce qu'un fichier binaire dans l'URL, c'est pas super
+    - On utilisera enctype avec la valeur multipart/form-data pour encoder la requête différemment
+    -->
+    <form method="POST" enctype="multipart/form-data">
+        <label for="titre">Titre</label>
+        <input type="text" name="titre" id="titre">
+
+        <label for="fichier">Choisir un fichier</label>
+        <input type="file" name="fichier" id="fichier">
+
+        <input type="submit" value="Envoyer le fichier">
+    </form>
+</body>
+</html>
