02af9737a3ef59105401203fb60b35273ecacbfe
Aboubakr <aboubakr.zennir@gmail.com>
2024-02-29 14:27:14 +0100

    envoyer fichier pas fini

--- envoyer.php
+++ envoyer.php
@@ -3,49 +3,12 @@ session_start();
 if (!isset($_SESSION['auth'])) {
     header('Location: index.php');
 }
+
 require('./HeaderFooter/Header.php');
-require('./Function/ft_extension.php');
-
-$fichier = $_FILES['fichier'];
-
-$_SESSION['errorMessage'] = "";
-if ($fichier['error'] == UPLOAD_ERR_OK) { // UPLOAD_ERR_OK est égale à 0
-    // Ca, ça veut dire que l'upload s'est bien passé
-    // Maintenant, on vérifie des infos sur le fichier uploadé
-    $parties = explode('.', $fichier['name']); // ['fichier', 'pdf']
-    $extension = $parties[1]; // 'pdf'
-    if (getAndVerify("." . $extension)) {
-        // C'est bon, c'est bien un fichier PDF
-        // On va quand même vérifier le contenu du fichier pour être certain(e)
-        // On ouvre le fichier tmp_name pour regarder ce qu'il y a dedans
-        $finfo = finfo_open(FILEINFO_MIME);
-        $info = finfo_file($finfo, $fichier['tmp_name']);
-
-        if (str_contains($info, 'application/pdf')) {
-            // Là on est certain(e)s que c'est vraiment du PDF
-
-            // Maintenant on vérifie que la taille est OK
-            $taille = filesize($fichier['tmp_name']);
-            if ($taille < 99999999) {
-                // La taille est en dessous de la limite, c'est ok
-
-                // Enfin, on peut déplacer le fichier.
-                $hash_email = hash('sha256', $_SESSION["email"]);
-                move_uploaded_file($fichier['tmp_name'], "./Upload" . "/" . $hash_email . '.pdf');
-            } else {
-                $_SESSION['errorMessage'] = "Aie ! Le fichier est trop volumineux";
-            }
-        } else {
-            $_SESSION['errorMessage'] = "Je t'ai vu, pirate !";
-        }
-    } else {
-        $_SESSION['errorMessage'] = "Le fichier n'est pas valide";
-    }
-} else {
-    // Il y a eu une erreur à l'envoi du fichier
-    $_SESSION['errorMessage'] = "Oups";
-}
 ?>
+
+
+<h1 style='text-align:center; '>Envoyer un fichier</h1>
 
 <?php
 if (isset($_SESSION['errorMessage'])) { ?>
@@ -55,14 +18,12 @@ if (isset($_SESSION['errorMessage'])) { ?>
 <?php unset($_SESSION['errorMessage']); // Supprime le message d'erreur de la session
 } ?>
 
-<h1 style='text-align:center; '>Envoyer un fichier</h1>
-
 <!--
     Pour envoyer un fichier :
     - On utilisera la méthode POST parce qu'un fichier binaire dans l'URL, c'est pas super
     - On utilisera enctype avec la valeur multipart/form-data pour encoder la requête différemment
     -->
-<form method="POST" enctype="multipart/form-data">
+<form method="POST" enctype="multipart/form-data" action="./Actions/EnvoyerFichierBdd.php">
     <label for="titre">Titre</label>
     <input type="text" name="titre" id="titre">
 
