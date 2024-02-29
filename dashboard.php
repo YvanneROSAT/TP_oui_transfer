c2844b827092b93f11fb68271be74c967c3bfca7
Stan97x <105353221+Stan97x@users.noreply.github.com>
2024-02-29 16:50:51 +0100

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -3,12 +3,37 @@ session_start();
 if (!isset($_SESSION['auth'])) {
     header('Location: index.php');
 }
+require('Actions/fichier.php');
+ $results = getheFichiers();
+ var_dump($results);
+
 require('./HeaderFooter/Header.php');
 ?>
 
 
 <h1 style='text-align:center; '>Bienvenu sur le Dashboard <?= $_SESSION['nom'] ?></h1>
+<table class="table">
+        <br>
+        <br>
+        <tr>
+        <th>nom_fichier</th>
+            <th>nom_fichier_cryptee</th>
+            <th>id_user</th>
+            <th>Action</th>         
+    </tr>
+    <?php while ($r = $results->fetch(PDO::FETCH_ASSOC)) {
 
+?>
+<tr>
+    <td><?php echo $r['nom_fichier'] ?></td>
+    <td><?php echo $r['nom_fichier_cryptee'] ?></td>
+    <td><?php echo $r['id_user'] ?></td>
+    <td>
+    <a href="edit.php?id=<?php echo $r['id_user'] ?>" class="btn btn-warning"download="<?php echo $r['id_user'] ?>">download</a>
+    <a onclick="return confirm('Are you sure you want to delete this rental?');" href="delete.php?id=<?php echo $r['id_user'] ?>" class="btn btn-danger">Delete</a>
+    </td>
+    <?php }
+    ?>
 <?php
 require('./HeaderFooter/Footer.php');
 ?>
---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['auth'])) {
+    header('Location: index.php');
+}
+require('Actions/fichier.php');
+$id = $_GET['id'];
+//Call Delete function
+$result = DeleteFichiers($id);
+//Redirect to list
+if ($result) {
+    header("Location: dashboard.php");
+} else {
+    echo"erreur";
+}
+?>  
