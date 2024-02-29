eae3624a6eee4d4990c585263af9fdc81c046864
Stan97x <105353221+Stan97x@users.noreply.github.com>
2024-02-29 16:51:38 +0100

    Add files via upload

---
+++ Actions/fichier.php
@@ -0,0 +1,48 @@
+<?php
+
+require('Databases.php');
+
+
+ function getheFichiers()
+    {
+        try {
+            $bdd=connexion();
+            $sql = "SELECT `nom_fichier`, `nom_fichier_cryptee`,id_user FROM `Fichiers` , Users WHERE Fichiers.id_user=Users.ID";
+            $result =$bdd->query($sql);
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }   
+
+    function DeleteFichiers($id)
+    {
+        try {
+            $bdd=connexion();
+            $sql = "DELETE FROM `Fichiers` WHERE id_user=:id";
+            $stmt =$bdd->prepare($sql);
+            $stmt->bindparam(':id', $id);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    function DownloadFichiers($id)
+    {
+        try {
+            $bdd=connexion();
+            $sql = "SELECT  FROM `Fichiers` WHERE id_user=:id";
+            $stmt =$bdd->prepare($sql);
+            $stmt->bindparam(':id', $id);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+    
