b7964d91bd212c8b923cb03c70ce81cfef2b0f92
Aboubakr <aboubakr.zennir@gmail.com>
2024-03-03 11:59:10 +0100

    modification du footer

--- HeaderFooter/Footer.php
+++ HeaderFooter/Footer.php
@@ -11,7 +11,7 @@
 <footer class="bg-dark text-light py-4 mt-auto">
     <div class="container-fluid text-center">
         <p><a href="#" class="text-light">Mentions légales</a> | <a href="#" class="text-light">Politique de confidentialité</a></p>
-        <p class="mb-0">© 2024 Votre Nom de Site. Tous droits réservés.</p>
+        <p class="mb-0">© 2024 Oui Transfer. Tous droits réservés.</p>
     </div>
 </footer>
 
